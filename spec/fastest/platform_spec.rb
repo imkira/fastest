a08701f6ebf9ceff28a2da56d7ed657a68fc10fa
Mario Freitas <imkira@gmail.com>
2011-06-22 12:12:51 +0900

    add dirs to loadpath in a cleaner way

--- spec/fastest/platform_spec.rb
+++ spec/fastest/platform_spec.rb
@@ -7,16 +7,38 @@ module Fastest
     end
 
     describe ".instance" do
-      it "should return a platform instance" do
+      it "should return the same object for multiple calls" do
+        plat2 = Platform.instance
+        plat3 = Platform.instance
+        plat2.should === subject
+        plat3.should === subject
+      end
+
+      it "should build a concrete platform" do
         should be_kind_of Platform
       end
     end
 
-    describe "#mac?" do
-      it "should return true on a mac" do
-        next unless subject.kind_of? Mac
-        should be_kind_of Mac
+    describe "#linux?" do
+      it "should return true on a Linux OS" do
+        next unless subject.kind_of? Linux
+        should be_kind_of Linux
       end
     end
+
+    describe "#lmac?" do
+      it "should return true on a Mac OS" do
+        next unless subject.kind_of? Mac
+        should be_kind_of Mac 
+      end
+    end
+
+    describe "#windows?" do
+      it "should return true on a Windows OS" do
+        next unless subject.kind_of? Windows
+        should be_kind_of Windows
+      end
+    end
+
   end
 end
