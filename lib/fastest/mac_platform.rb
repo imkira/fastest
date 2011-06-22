810060ec2cc39ca606dd67288201a1e93e32890d
Mario Freitas <imkira@gmail.com>
2011-06-22 15:00:00 +0900

    dummy default_* methods added

--- lib/fastest/mac_platform.rb
+++ lib/fastest/mac_platform.rb
@@ -7,5 +7,15 @@ module Fastest
 
     def initialize
     end
+
+    # @return [String] path to default browser application
+    def default_browser
+      raise 'not implemented'
+    end
+
+    # @return [String] path to default (graphical) text editor
+    def default_graphical_text_editor
+      raise 'not implemented'
+    end
   end
 end
