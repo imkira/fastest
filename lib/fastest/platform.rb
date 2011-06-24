05bbc87f6b1bb293edc97ad98d416a4fc260b079
Mario Freitas <imkira@gmail.com>
2011-06-24 14:38:01 +0900

    yard doc and exception class change

--- lib/fastest/platform.rb
+++ lib/fastest/platform.rb
@@ -1,11 +1,15 @@
 require 'singleton'
 
 module Fastest
+  # Generic Platform class.
+  # @abstract
   class GenericPlatform
     include Singleton
 
-    # @return [Module]
-    # The concrete platform namespace for the current platform.
+    # Returns the namespace (Module) under which the concrete
+    # implementations for the current platform can be found.
+    #
+    # @return [Module] the platform namespace for the current platform
     def self.namespace
       @namespace ||=
         case RUBY_PLATFORM
@@ -22,30 +26,35 @@ module Fastest
           require 'fastest/windows/platform'
           Fastest::Windows
         else
-          raise UnknownPlatformError, "Unknown platform: #{platform}"
+          raise Exception::UnknownPlatform, "Unknown platform: #{platform}"
         end
     end
 
+    # Checks whether the platform refers to a Unix-like system
     # @return [true, false] true on Unix (including MacOS), false otherwise
     def unix?
       kind_of? Unix::Platform rescue false
     end
 
+    # Checks whether the platform refers to a Linux system
     # @return [true, false] true on Linux, false otherwise
     def linux?
       kind_of? Unix::Platform rescue false
     end
 
+    # Checks whether the platform refers to a BSD-like system
     # @return [true, false] true on BSD (including MacOS), false otherwise
     def bsd?
       kind_of? BSD::Platform rescue false
     end
 
+    # Checks whether the platform refers to a MacOS system
     # @return [true, false] true on MacOS, false otherwise
     def mac?
       kind_of? Mac::Platform rescue false
     end
 
+    # Checks whether the platform refers to a Windows system
     # @return [true, false] true on Windows, false otherwise
     def windows?
       kind_of? Windows::Platform rescue false
