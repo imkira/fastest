52bb986daea666142acbbd7c9a78e7e6506b5b03
Mario Freitas <imkira@gmail.com>
2011-06-24 14:30:25 +0900

    place all exceptions under Exception module

--- lib/fastest/exceptions.rb
+++ lib/fastest/exceptions.rb
@@ -1,3 +1,7 @@
 module Fastest
-  class UnknownPlatformError < Exception; end
+  # This module acts as a namespace for all Exception classes
+  module Exception
+    # 'Unknown platform detected' exception class
+    class UnknownPlatform < Exception; end
+  end
 end
