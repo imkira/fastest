fb97681ccc0d95aee2bbb0df3c9ee30a9f115bc5
Mario Freitas <imkira@gmail.com>
2011-06-24 14:41:11 +0900

    initial implementation of GenericProcess

--- lib/fastest/process.rb
+++ lib/fastest/process.rb
@@ -1,4 +1,49 @@
 module Fastest
+  # Generic Process class.
+  # @abstract
   class GenericProcess
+    include Comparable
+
+    # @return [Fixnum] the process id
+    attr_reader :pid
+
+    # @return [Time] the process creation time
+    attr_reader :created_at
+
+    # @return [Fixnum] the parent process id
+    attr_reader :ppid
+
+    # @return [String] the full process path
+    attr_reader :path
+
+    # @return [String] the command line passed to the process
+    attr_reader :cmd_line
+
+    def initialize (pid, created_at, ppid = nil, path = nil, name = nil, cmd_line = nil)
+      @pid = pid
+      @created_at = created_at
+      @ppid = ppid
+      @path = path
+      @name = name
+      @cmd_line = cmd_line
+    end
+
+    # Returns the current process object
+    # @return [GenericProcess] the process object for the current process
+    def self.current
+      all[::Process.pid]
+    end
+
+    # Compares receiver process against another
+    # @param [GenericProcess] target process to compare against
+    # @return [-1, 0, 1] implementation of Comparable's <=>
+    def <=> (process2)
+      cmp = (pid <=> process2.pid)
+      if cmp != 0
+        cmp
+      else
+        created_at <=> process2.created_at
+      end
+    end
   end
 end
