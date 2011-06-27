607aa63189974e18800fa9042bee64076cf47e36
Mario Freitas <imkira@gmail.com>
2011-06-27 10:53:38 +0900

    fixed describe

--- spec/fastest/platform_spec.rb
+++ spec/fastest/platform_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 module Fastest
-  describe 'Platform' do
+  describe GenericPlatform do
     subject do
       Platform
     end
