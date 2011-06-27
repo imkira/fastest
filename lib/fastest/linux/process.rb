4cd515035e92302bf8b26c8da337eace4c791f6c
Mario Freitas <imkira@gmail.com>
2011-06-27 16:34:14 +0900

    simplified process by using sys-* gems

--- lib/fastest/linux/process.rb
+++ lib/fastest/linux/process.rb
@@ -2,53 +2,28 @@ module Fastest
   module Linux 
     # Process class for the Linux platform (see {GenericProcess})
     class Process < Fastest::Unix::Process
+      # Returns the current process object
+      # @return [GenericProcess] the process object for the current process
+      def self.current
+        sys_process_to_process Sys::ProcTable.ps(::Process.pid)
+      end
+
       # Retrieve all currently executing processes
       # @return [Hash] hash of currently running processes indexed by their PID
       def self.all
-        Dir.entries('/proc').inject({}) do |procs, pid|
-          # ignore non-directories
-          next procs unless File.directory?(path_to_proc_dir(pid))
-          # ignore directories whose name are not numbers
-          next procs unless pid =~ /^\d+$/
-          # create process object
-          pid = pid.to_i
-          path = read_proc_path(pid)
-          cmd_line = read_proc_cmd_line(pid)
-          procs[pid] = Process.new(pid, nil, nil, path, cmd_line)
+        Sys::ProcTable.ps.inject({}) do |procs, process|
+          procs[process.pid] = sys_process_to_process(process)
           procs
         end
       end
 
       private
 
-      # Compute path to process information directory
-      # @param [Fixnum] the pid number of the process to inspect
-      # @return [String] the path to the folder of the given process information
-      def self.path_to_proc_dir (pid)
-        File.expand_path(File.join('/proc', pid.to_s))
-      end
-
-      # Read the full process path
-      # @param [Fixnum] the pid number of the process to inspect
-      # @note This method may return nil if you don't have permission to read the process path
-      # @return [String, nil] the full path to the process or nil (e.g., permission denied)
-      def self.read_proc_path (pid)
-        exe_path = File.join(path_to_proc_dir(pid), 'exe')
-        begin
-          if File.symlink? exe_path
-            File.expand_path(File.readlink(exe_path))
-          end
-        rescue
-        end
-      end
-
-      # Read the command line the process was invoqued with
-      # @param [Fixnum] the pid number of the process to inspect
-      # @note This method may return nil if you don't have permission to read the command line
-      # @return [String, nil] the full path to the process or nil (e.g., permission denied)
-      def self.read_proc_cmd_line (pid)
-        cmdline_path = File.join(path_to_proc_dir(pid), 'cmdline')
-        File.read(cmdline_path)
+      def self.sys_process_to_process (process)
+        # starttime as reported by /proc is the number of jiffies since last boot
+        created_at = Fastest::Platform.system_boot_time
+        created_at += process.starttime / Fastest::Platform.system_clock_tick.to_f
+        Process.new(process.pid, created_at, process.ppid, process.exe, process.cmdline)
       end
     end
   end
