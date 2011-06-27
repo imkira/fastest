368a384320f84ba9066c9f63943ccb750d52f119
Mario Freitas <imkira@gmail.com>
2011-06-27 16:33:52 +0900

    added support for LibC through FFI

--- lib/fastest/linux/platform.rb
+++ lib/fastest/linux/platform.rb
@@ -15,6 +15,20 @@ module Fastest
       def default_text_editor
         `update-alternatives --list gnome-text-editor`.lines.first.chomp
       end
+
+      # Returns the HZ
+      # @return [Fixnum] clock tick for the current kernel
+      def system_clock_tick
+        @@system_clock_tick ||= LibC::sysconf(LibC::SC_CLK_TCK)
+      end
+    end
+
+    module LibC
+      extend FFI::Library
+      ffi_lib FFI::Library::LIBC
+      attach_function :sysconf, [:int], :long
+
+      SC_CLK_TCK = 2
     end
   end
 end
