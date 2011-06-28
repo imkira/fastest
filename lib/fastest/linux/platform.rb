3d99a80b2503caae9fdd2b85bdd87d719d70dd55
Mário Freitas <imkira@gmail.com>
2011-06-28 16:02:38 +0900

    force expand_path on default apps

--- lib/fastest/linux/platform.rb
+++ lib/fastest/linux/platform.rb
@@ -8,12 +8,12 @@ module Fastest
     class Platform < Fastest::Unix::Platform
       # @return [String] path to default browser application
       def default_browser
-        `update-alternatives --list x-www-browser`.lines.first.chomp
+        File.expand_path(`update-alternatives --list x-www-browser`.lines.first.chomp)
       end
 
       # @return [String] path to default (graphical) text editor
       def default_text_editor
-        `update-alternatives --list gnome-text-editor`.lines.first.chomp
+        File.expand_path(`update-alternatives --list gnome-text-editor`.lines.first.chomp)
       end
 
       # Returns the system clock's number of ticks (jiffies) per second
