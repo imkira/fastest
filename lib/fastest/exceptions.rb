f85a995b59568e8750220e50904c88709328d78d
Mário Freitas <imkira@gmail.com>
2011-06-29 18:31:10 +0900

    new UnknownProcessState exception class

--- lib/fastest/exceptions.rb
+++ lib/fastest/exceptions.rb
@@ -3,5 +3,7 @@ module Fastest
   module Exception
     # 'Unknown platform detected' exception class
     class UnknownPlatform < ::Exception; end
+    # 'Unknown process state' exception class
+    class UnknownProcessState < ::Exception; end
   end
 end
