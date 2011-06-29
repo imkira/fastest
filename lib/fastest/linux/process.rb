69918d7fb28897b782454c71fc3b602fb157ce53
Mário Freitas <imkira@gmail.com>
2011-06-29 18:32:37 +0900

    added state method

--- lib/fastest/linux/process.rb
+++ lib/fastest/linux/process.rb
@@ -3,6 +3,45 @@ module Fastest
     # Process class for the Linux platform (see {GenericProcess})
     class Process < Fastest::Unix::Process
 
+      protected
+
+      # Returns the state of the process
+      # @return [Hash] hash of symbols which represent the state
+      def state
+        process = Sys::ProcTable.ps(@pid)
+        state_conds = { :running => true }
+        unless process.nil?
+          case process.state
+          when 'D'
+            # uninterruptible sleep (usually IO)
+            state_conds[:sleeping] = true
+          when 'R'
+            # running or runnable (on run queue)
+          when 'S'
+            # interruptible sleep (waiting for an event to complete)
+            state_conds[:sleeping] = true
+          when 'T'
+            # stopped, either by a job control signal or because it is being traced
+            # this also happens when you do a CTRL-Z on a foreground process
+            state_conds[:stopped] = true
+          when 'W'
+            # paging (not valid since the 2.6.xx kernel)
+          when 'X'
+            # dead (should never be seen)
+            state_conds[:running] = false
+          when 'Z'
+            # defunct ("zombie") process, terminated but not reaped by its parent
+            state_conds[:stopped] = true
+          else
+            raise Exception::UnknownProcessState, "Unknown process state: #{process.state}"
+          end
+        else
+          # process not found
+          state_conds[:running] = false
+        end
+        state_conds
+      end
+
       private
 
       # Convert a Struct::ProcTableStruct entry into a Fastest::Process
