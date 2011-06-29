302daf0f48c74af566bb2a3de652569a81a74a7c
Mário Freitas <imkira@gmail.com>
2011-06-29 16:15:53 +0900

    use bundler

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,16 @@
 $:<<(File.join(File.dirname(__FILE__), '..', 'lib'))
 $:<<(File.dirname(__FILE__))
-require 'rspec'
+
+require 'rubygems'
+require 'bundler'
+Bundler.require(:development)
 require 'fastest'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir['support/**/*.rb'].each do |f|
+  require f
+end
 
 RSpec.configure do |config|
   
